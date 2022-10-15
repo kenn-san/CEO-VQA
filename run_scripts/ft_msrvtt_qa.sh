87617da7011986c6d6a432e97ec1482dca023f72
scysy1 <scysy1@nottingham.edu.cn>
2022-10-15 19:33:10 +0800

    [Msic] adjust configs

--- run_scripts/ft_msrvtt_qa.sh
+++ run_scripts/ft_msrvtt_qa.sh
@@ -7,4 +7,4 @@ CONFIG_PATH='config_release/msrvtt_qa.json'
 
 horovodrun -np 1 python src/tasks/run_video_qa.py \
       --config $CONFIG_PATH \
-      --output_dir output/ovqa_baseline/20221014_test
+      --output_dir output/ovqa_baseline/20221015_test
